d23e81a712d4dbf675f3fe650439de8d753f03ed
Brazen <764146802@qq.com>
2019-07-24 23:04:58 +0800

    修改头像背景颜色

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-let qrSwitch = document.querySelector('.qrSwitch')
-let qrCode = document.querySelector('.qrCode')
+// let qrSwitch = document.querySelector('.qrSwitch')
+// let qrCode = document.querySelector('.qrCode')
 
-qrSwitch.addEventListener('click', (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-    qrCode.classList.add('active')
-    document.addEventListener('click', qrClose)
-})
+// qrSwitch.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     e.stopPropagation()
+//     qrCode.classList.add('active')
+//     document.addEventListener('click', qrClose)
+// })
 
 
-function qrClose() {
-    qrCode.classList.remove('active')
-    document.removeEventListener('click', qrClose)
-}
+// function qrClose() {
+//     qrCode.classList.remove('active')
+//     document.removeEventListener('click', qrClose)
+// }
